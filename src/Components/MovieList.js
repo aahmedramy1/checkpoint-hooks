4b8316288aa3f76ada557e7bd2e7a5bd5750a76f
Ahmed Ramy <ahmedramy.e404@gmail.com>
2021-08-16 10:17:33 +0200

    added space

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -16,7 +16,7 @@ export default function MovieList({movies, term, filter})
 			}).map((movie, index) => (
 				<div style = {{width: "18rem", margin: "30px"}} key ={index}>
 					<Link to = {`/${movie.movieID}`} style = {{textDecoration : "none", color: "black"}}  >
-						<MovieCard title = {movie.Title} imageURL = {movie.Poster} description = {movie.Description.substring(0, 50) + "...."} rating = {movie.rating}  />
+						<MovieCard title = {movie.Title} imageURL = {movie.Poster} description = {movie.Description.substring(0, 50) + " ...."} rating = {movie.rating}  />
 					</Link>
 				</div>
 			))}
